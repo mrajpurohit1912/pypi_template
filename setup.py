6164006e19e11482af252615d47ae5259cc288d9
mrajpurohit1912 <mrajpurohit1912@gmail.com>
2024-12-12 15:11:05 +0530

    removed unwanted files

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md","r",encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "IPYNBrenderer"
-AUTHOR_USER_NAME = "Mahavir Rajpurohit"
-SRC_REPO = "IPYNBrenderer"
-AUTHOR_EMAIL = "mrajpurohit1912@gmail.com"
+REPO_NAME = "<YOUR REPO NAME>"
+AUTHOR_USER_NAME = "<YOUR USERNAME>"
+SRC_REPO = "<YOUR REPO NAME>"
+AUTHOR_EMAIL = "<YOUR EMAIL ID>"
 
 setuptools.setup(
     name=SRC_REPO,
--- template.py
+++ template.py
@@ -18,18 +18,18 @@ logging.info(f"Creating project by name: {project_name}")
 #list of files
 
 list_of_files = [
-    ".github/workflows/.gitkeep",
+    # ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    "tests/__init__.py",
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
-    "init_setup.sh",
-    "requirements.txt",
-    "requirements_dev.txt",
-    "setup.py"
-    "pyproject.toml",
-    "setup.cfg",
-    "tox.ini"
+    # "tests/__init__.py",
+    # "tests/unit/__init__.py",
+    # "tests/integration/__init__.py",
+    # "init_setup.sh",
+    # "requirements.txt",
+    # "requirements_dev.txt",
+    # "setup.py"
+    # "pyproject.toml",
+    # "setup.cfg",
+    # "tox.ini"
 ]
 
 for filepath in list_of_files:
